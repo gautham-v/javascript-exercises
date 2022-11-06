e7fbff9be5eff7e46e720afb3b8235ff4f621da4
gautham-v <gautham.vemulapalli@gmail.com>
2022-11-06 13:57:34 -0500

    completed second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let newstr = '';
+    if (num < 0){
+        newstr += 'ERROR';
+    }
+    else {
+        for (let i=0; i<num; i++) {
+            newstr += str;
+        }
+    }
+    
+    return newstr;
 };
 
 // Do not edit below this line
