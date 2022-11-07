bacf39d8d472ed7fbafa5e93dcce4f72e09832f2
gautham-v <gautham.vemulapalli@gmail.com>
2022-11-07 18:03:42 -0500

    completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array = string.split('');
+    var reverse = '';
+    console.log(array);
+    while (array.length>0) {
+        reverse += array.pop();
+    }
+    return reverse;
 };
 
 // Do not edit below this line
