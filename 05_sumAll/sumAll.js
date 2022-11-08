126ec294ff5a23b14445d3ee3543e2a7ca7c420c
gautham-v <gautham.vemulapalli@gmail.com>
2022-11-07 19:07:21 -0500

    completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+    if ((start < 0) || (end < 0) || (typeof start != "number") || (typeof end != "number")){
+        return 'ERROR';
+    }
+    else {
+        let finalSum = 0;
+        if (start > end){
+            [start, end] = [end, start];
+        }
+        for (let i = start; i <= end; i++){
+            finalSum += i;
+        }
+        return finalSum;
+    }
 };
 
 // Do not edit below this line
