e9360928a3f710d2a2cbdf63b7d7e79fd9757262
gautham-v <gautham.vemulapalli@gmail.com>
2022-11-07 20:45:54 -0500

    completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+
+  //(x-32)*(5/9)
+  celcius = (temp-32)*(5/9)
+  celcius = Math.round(celcius*10)/10
+  return celcius;
 
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  fahrenheit = (temp*(9/5)+32);
+  fahrenheit = Math.round(fahrenheit*10)/10
+  return fahrenheit
 };
 
 // Do not edit below this line
