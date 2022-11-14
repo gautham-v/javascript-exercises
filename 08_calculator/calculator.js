a48c00696d47639abd93f57f91cf6414d4beb477
Gautham Vemulapalli <gautham.vemulapalli@gmail.com>
2022-11-14 10:30:52 -0500

    completed calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,53 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce(function(sum,item){
+    if (array.length === 0){
+      return 0;
+    }
+    else {
+      return sum += item;
+    }
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce(function(product,item){
+    if (array.length === 0){
+      console.log(array.length);
+      return 0;
+    }
+    else {
+      return product *= item;
+    }
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	let array = [];
+  for (let i = 1; i <= b; i++){
+    array.push(a);
+  }
+  return multiply(array);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0) {
+    return 1;
+  }
+  else {
+    let array = [];
+    for (let i = a; i >= 1; i--){
+      array.push(i);
+    }
+    return multiply(array);
+  }
 };
 
 // Do not edit below this line
