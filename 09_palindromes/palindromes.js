d3754eb0d1579cebaffc59ca778d46566a8aa865
Gautham Vemulapalli <gautham.vemulapalli@gmail.com>
2022-11-14 13:11:54 -0500

    completed palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,41 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let firstHalf = [];
+    let secondHalf = [];
 
+    // remove punctuation, whitespace and to lower case
+    str = str.replaceAll(' ', '');
+    str = str.replaceAll(',', '');
+    str = str.replaceAll('!', '');
+    str = str.replaceAll('.', '');
+    str = str.toLowerCase();
+
+    console.log(str);
+
+    const length = str.length;
+    const midpoint = length/2;
+
+    if (length%2 === 1){ //odd
+        firstHalf = Array.from(str.slice(0,Math.floor(midpoint)));
+        secondHalf = Array.from(str.slice(Math.ceil(midpoint)));
+    }
+    else{ //even
+        firstHalf = Array.from(str.slice(0,midpoint));
+        secondHalf = Array.from(str.slice(midpoint));
+    }
+
+    console.log(firstHalf);
+    console.log(secondHalf);
+    for (let i = 0; i<= midpoint-1; i++){
+        console.log(firstHalf[i]);
+        console.log(secondHalf[Math.floor(midpoint)-1-i]);
+        if (firstHalf[i] !== secondHalf[Math.floor(midpoint)-1-i]){
+            return false;
+        }
+    }
+    return true;
 };
+
+//palindromes('ZZZZ car, a man, a maracaz.')
 
 // Do not edit below this line
 module.exports = palindromes;
