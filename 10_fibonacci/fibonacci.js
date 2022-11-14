97d49da68821c46d8218708625de04a4ec6b08fe
Gautham Vemulapalli <gautham.vemulapalli@gmail.com>
2022-11-14 13:44:21 -0500

    completed fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
 
+    if (index < 1){
+        return 'OOPS';
+    }
+    if (typeof(index) === 'string')
+    {
+        index = Number(index);
+    }
+    let array = [1,1];
+    let firstNum = array[array.length-2];
+    let secondNum = array[array.length-1];
+    
+    while (array.length <= index){
+        array.push(firstNum+secondNum);
+        firstNum = array[array.length-2];
+        secondNum = array[array.length-1]; 
+    }
+
+    return firstNum;
 };
 
+console.log(fibonacci(4));
 // Do not edit below this line
 module.exports = fibonacci;
