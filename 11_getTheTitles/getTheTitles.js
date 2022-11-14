edc59fb45c87c14156e7ed63a60114cced6b63d2
Gautham Vemulapalli <gautham.vemulapalli@gmail.com>
2022-11-14 14:24:49 -0500

    completed exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+    
+    let listOfBooks = [];
+    array.forEach(element => {
+        listOfBooks.push(element.title);
+    });
 
+    return listOfBooks;
 };
 
 // Do not edit below this line
